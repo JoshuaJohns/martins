f7f6587e61deb21b7e6817d6738a75ca40af7f69
Joshua Johns <joshuajohns28@outlook.com>
2022-08-11 15:45:43 -0500

    commiting

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let fishData = (Name, Species, Color ) => {
+    return {
+        Name,
+        Species,
+        Color,
+    }
+};
+
+let shark = fishData(GrreatWhite, Shark, Grey);
+console.log(shark);
